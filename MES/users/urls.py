3d138b75855362569b0d87bd8e63813b81f497af
BasterPM <ip.petrovskiy.cnc@gmail.com>
2025-01-06 02:11:26 +0300

    Написание системы логининга, логаута, регистрации Создание необходимых форм, представлений и шаблонов Настройка адресов

---
+++ MES/users/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import start_page_view, register_view, dashboard_view, logout_view
+
+urlpatterns = [
+    path('', start_page_view, name='start_page'),
+    path('register/', register_view, name='register'),
+    path('dashboard/', dashboard_view, name='dashboard'),
+    path('logout/', logout_view, name='logout'),
+]
--- MES/users/views.py
+++ MES/users/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from .forms import CustomUserCreationForm, AuthenticationForm
 
-# Create your views here.
+
+def start_page_view(request):
+    if request.user.is_authenticated:
+        return redirect('dashboard')  # Личный кабинет
+    else:
+        if request.method == 'POST':
+            form = AuthenticationForm(data=request.POST)
+            if form.is_valid():
+                username = form.cleaned_data.get('username')
+                password = form.cleaned_data.get('password')
+                user = authenticate(request, username=username, password=password)
+                if user is not None:
+                    login(request, user)
+                    return redirect('dashboard')  # Перенаправление в личный кабинет
+        else:
+            form = AuthenticationForm()
+        return render(request, 'login.html', {'form': form})  # Окно авторизации
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('start_page')  # Перенаправление на гравную страницу
+
+
+def register_view(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # авторизуем пользователя после регистрации
+            return redirect('dashboard.html')  # Перенаправление на главную страницу
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'register.html', {'form': form})
+
+
+def dashboard_view(request):
+    return render(request, 'dashboard.html')
